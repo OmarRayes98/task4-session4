53dea5082f843b762aae25e0600abee921978536
Omar Rayes <omar.rayes@aventura.com>
2026-07-13 22:45:59 +0300

    config database

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
+import dotenv from "dotenv";
+
 import express from "express";
 import mongoose from "mongoose";
 
+
+dotenv.config();
 const app = express();
 const port = 3001;
 
-// mongoose
-//   .connect(process.env.DATABASE_URL || "")
-//   .then(() => console.log("Mongo connected!"))
-//   .catch((err) => console.log("Failed to connect!", err));
+
+console.log(process.env.DATABASE_URL,"..");
+mongoose
+  .connect(process.env.DATABASE_URL || "")
+  .then(() => console.log("Mongo connected!"))
+  .catch((err) => console.log("Failed to connect!", err));
 
 
 
